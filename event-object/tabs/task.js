2939457ab71da97852e5f7e7e3d9126002b943c0
AlexanderAndreevGIT <andreev-alex@inbox.ru>
2020-12-09 22:57:10 +0300

    first commit event-object tabs

---
+++ event-object/tabs/task.js
@@ -0,0 +1,15 @@
+"use strict"
+
+const tabs = Array.from(document.getElementsByClassName('tab'));
+const tabsContent = Array.from(document.getElementsByClassName('tab__content'))
+let activeIndex = 0;
+
+function switchContent() {
+	tabs[activeIndex].classList.remove('tab_active');
+	tabsContent[activeIndex].classList.remove('tab__content_active');
+	activeIndex = tabs.indexOf(this);
+	tabs[activeIndex].classList.add('tab_active');
+	tabsContent[activeIndex].classList.add('tab__content_active');
+}
+
+tabs.forEach(item => item.addEventListener("click", switchContent))
