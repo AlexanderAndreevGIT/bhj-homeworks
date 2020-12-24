5f234d3ec7a729901d696182a22d85cb64ee0e1e
AlexanderAndreevGIT <andreev-alex@inbox.ru>
2020-12-24 18:13:28 +0300

    second commit document-structure tooltip

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -3,7 +3,7 @@
 const tooltips = Array.from(document.getElementsByClassName('has-tooltip'));
 let activeTip;
 
-function addTooltip (item) {
+function addTooltip(item) {
 	let tip = `<div class="tooltip">
       ${item.title}
     </div>` 
@@ -13,18 +13,17 @@ function addTooltip (item) {
 tooltips.forEach(item => item.insertAdjacentHTML('beforeEnd', addTooltip(item)))
 
 function showHideTooltip(e) {
-	e.preventDefault()
-
-	let tipToChange = this.querySelectorAll('.tooltip')[0]
+	e.preventDefault();
+	let tipToChange = this.querySelectorAll('.tooltip')[0];
 	if (activeTip != undefined) {
 		if (tipToChange.textContent != activeTip.textContent) {
 			activeTip.classList.remove('tooltip_active');
 		}
 	}
 	tipToChange.classList.toggle('tooltip_active');
-	tipToChange.style = `margin-left: ${this.getBoundingClientRect().left}px;`
+	tipToChange.style = `margin-left: ${this.getBoundingClientRect().left}px;`;
 	activeTip = tipToChange;
-	return activeTip
+	return activeTip;
 }
 
 tooltips.forEach(item => item.addEventListener('click', showHideTooltip));
